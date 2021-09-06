aefb575a37dbaa31c1bee3ebf2910b496d9432c2
Nieblas <francisco_nieblas3@hotmail.com>
2021-09-06 14:26:19 -0600

    commit

--- EJEMPLOS2/assertions.js
+++ EJEMPLOS2/assertions.js
@@ -3,3 +3,5 @@ var longitudDelString = algunValor.length;
 var algunValor2 = "Este es un string";
 var longitudDelString2 = algunValor2.length;
 //------------------------------------
+
+//prueba git
