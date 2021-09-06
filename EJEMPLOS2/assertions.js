ece76bf1b8926f9d773fb6483fc2466a37a5d78f
Nieblas <francisco_nieblas3@hotmail.com>
2021-09-06 14:29:20 -0600

    commit on master

--- EJEMPLOS2/assertions.js
+++ EJEMPLOS2/assertions.js
@@ -3,5 +3,3 @@ var longitudDelString = algunValor.length;
 var algunValor2 = "Este es un string";
 var longitudDelString2 = algunValor2.length;
 //------------------------------------
-
-//prueba git
--- EJEMPLOS2/enums.ts
+++ EJEMPLOS2/enums.ts
@@ -8,3 +8,5 @@ let tacoma: marcasDeAutos = marcasDeAutos.Toyota;
 
 console.log(tacoma);
 console.log(marcasDeAutos[0]);
+
+//prueba git
